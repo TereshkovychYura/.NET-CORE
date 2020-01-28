2805ad5c301f8f9f895e37f866dd3e321d1c4420
Юрій Терешкович <Frez70@rivne.itstep.org>
2020-01-28 11:55:53 +0200

    first commit

---
+++ IntroShop/IntroShop/Controllers/PhoneController.cs
@@ -0,0 +1,32 @@
+﻿using IntroShop.Main.Interfaces;
+using IntroShop.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace IntroShop.Controllers
+{
+    public class PhoneController : Controller
+    {
+        private readonly IAllPhones _allPhones;
+        private readonly IPhoneCategory _allCategories;
+
+        public PhoneController( IAllPhones iAllPhones , IPhoneCategory iAllCategories)
+        {
+            _allPhones = iAllPhones;
+            _allCategories = iAllCategories;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Phones";
+            PhoneListViewModel obj = new PhoneListViewModel();
+            obj.allPhones = _allPhones.Phones;
+            obj.currentCategory = "Best category";
+
+            return View(obj);
+        }
+    }
+}
