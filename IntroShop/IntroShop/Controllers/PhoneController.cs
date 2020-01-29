6130e472a88053a350363d29cddd3389a0248cf0
Юрій Терешкович <Frez70@rivne.itstep.org>
2020-01-29 11:45:33 +0200

    Add front part

--- IntroShop/IntroShop/Controllers/PhoneController.cs
+++ IntroShop/IntroShop/Controllers/PhoneController.cs
@@ -4,7 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace IntroShop.Controllers
 {
